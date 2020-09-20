2d127512c12803adb8e40d0a1f06bfefb3cd13c2
Kyle Johnson <kyleaj@princeton.edu>
2020-09-20 01:19:33 -0400

    Add project files.

---
+++ UnoBuildDebug/UnoBuildDebug.Shared/CustomControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace UnoBuildDebug.Shared
+{
+    public sealed partial class CustomControl : UserControl
+    {
+
+        public string Text
+        {
+            set => TextDisplay.Text = value;
+        }
+
+        public CustomControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
